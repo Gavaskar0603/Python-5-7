65990ee2e56365e255931694a3966ef413c98a2d
Gavaskar S <92905238+Gavaskar0603@users.noreply.github.com>
2026-07-06 14:44:39 +0530

    Add files via upload

---
+++ f.py
@@ -0,0 +1,9 @@
+'''for i in range(1,11,2):
+    print(i)
+'''
+num=int(input("enter the value"))
+for i in range(1,11,1):
+    print(num,"x",i,"=",i*num)
+
+for i in "keerthi":
+    print(i,end="")
---
+++ first_day.py
@@ -0,0 +1,110 @@
+'''
+keywords
+help("keywords")
+'''
+
+'''
+#indentation
+print("Praveen")
+
+#variables with datatype
+NAME = 'Python'
+age = 35
+Version = 3.14
+is_check = False
+complex2 = 6+3j
+
+print(NAME,type(NAME))
+print(age,type(age))
+print(Version,type(Version))
+print(is_check,type(is_check))
+print(complex2,type(complex2))
+'''
+
+
+#inputs
+'''
+name = input("enter your Name : ")
+print(name,type(name))
+
+age = float(input("enter your age : "))
+print(age,type(age))
+'''
+
+#operators
+#Arithmetic operator
+'''
+a = 5
+b = 2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b)
+print(a**3)
+'''
+
+#comparision operator
+'''
+a = 10
+b = 20
+c = 40
+d = 10
+
+print(a == b)
+print(a == d)
+print(c != a)
+print(a < b)
+print(c > b)
+print(a <= d)
+print(c >= b)
+'''
+
+
+n = 15
+print(n>3 and n<10)
+print(n==3 or n==10)
+print(not(n==3 or n!=10))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
